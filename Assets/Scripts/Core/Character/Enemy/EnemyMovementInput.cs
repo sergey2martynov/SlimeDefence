aec009cb75a93b3658f18f8725dc7809e5be48da
sergey2martynov <94121806+sergey2martynov@users.noreply.github.com>
2022-06-14 23:01:04 +0800

    Fixed damage for bosses

--- Assets/Scripts/Core/Character/Enemy/EnemyMovementInput.cs
+++ Assets/Scripts/Core/Character/Enemy/EnemyMovementInput.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System;
+using CodeBase.Core.Character.Player;
+using UnityEngine;
 using UnityEngine.AI;
 
 namespace CodeBase.Core.Character.Enemy
@@ -20,6 +22,14 @@ namespace CodeBase.Core.Character.Enemy
         public void MoveEnemy()
         {
             _agent.destination = _target.position;
+        }
+
+        private void OnTriggerEnter(Collider collision)
+        {
+            if (collision.TryGetComponent(out PlayerMovementInput player))
+            {
+                _agent.isStopped = true;
+            }
         }
     }
 }
